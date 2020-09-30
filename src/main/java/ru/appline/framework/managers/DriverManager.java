7f31cc866ccf10a75d3081959e78dcebe071b144
anton_balakirev <anton_balakirev@epage.ru>
2020-10-01 01:51:16 +0300

    add remote webdriver

--- src/main/java/ru/appline/framework/managers/DriverManager.java
+++ src/main/java/ru/appline/framework/managers/DriverManager.java
@@ -5,6 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 
 import static ru.appline.framework.utils.PropConst.*;
 
@@ -45,18 +50,23 @@ public class DriverManager {
                 System.setProperty("webdriver.gecko.driver", props.getProperty(PATH_GEKO_DRIVER));
                 driver = new FirefoxDriver();
                 break;
-            default:
+            case "chrome":
                 System.setProperty("webdriver.chrome.driver", props.getProperty(PATH_CHROME_DRIVER));
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
-                chromeOptions.addArguments("start-maximized");
-                chromeOptions.addArguments("enable-automation");
-                chromeOptions.addArguments("--no-sandbox");
-                chromeOptions.addArguments("--disable-infobars");
-                chromeOptions.addArguments("--disable-dev-shm-usage");
-                chromeOptions.addArguments("--disable-browser-side-navigation");
-                chromeOptions.addArguments("--disable-gpu");
-                driver = new ChromeDriver(chromeOptions);
+                driver = new ChromeDriver();
+                break;
+            case "remote":
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setBrowserName("chrome");
+                capabilities.setVersion("73.0");
+                capabilities.setCapability("enableVNC", true);
+                capabilities.setCapability("enableVideo", false);
+                try {
+                    driver = new RemoteWebDriver(
+                            URI.create("http://selenoid.appline.ru:4445/wd/hub/").toURL(),
+                            capabilities);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
         }
     }
 
