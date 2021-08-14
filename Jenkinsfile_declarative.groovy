09af2d701f10d05d517435863cd90a4c077ca705
anton_balakirev <anton_balakirev@epage.ru>
2021-08-14 10:08:13 +0300

    add scripted pipeline

--- Jenkinsfile_declarative.groovy
+++ Jenkinsfile_declarative.groovy
@@ -1,20 +1,20 @@
 def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
 
-pipeline{
+pipeline {
     agent any
-    stages{
-        stage ('Build') {
-            steps{
+    stages {
+        stage('Build') {
+            steps {
                 sh "${mvn} clean compile"
             }
         }
-        stage ('Run Tests'){
-            steps{
+        stage('Run Tests') {
+            steps {
                 sh "${mvn} test -Dcucumber.filter.tags=\"${TAG}\""
             }
         }
-        stage('Allure Report Generation'){
-            steps{
+        stage('Allure Report Generation') {
+            steps {
                 allure includeProperties: false,
                         jdk: '',
                         results: [[path: 'target/reports/allure-results']]
--- Jenkinsfile_scripted.groovy
+++ Jenkinsfile_scripted.groovy
@@ -1,3 +1,24 @@
-node('any') {
+def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
 
+node('any') {
+    stage('Build') {
+        sh "${mvn} clean compile"
+    }
+    stage('Run Tests') {
+        try {
+            sh "${mvn} test -Dcucumber.filter.tags=\"${TAG}\""
+        }
+        catch (Exception e) {
+            echo "Test run was broken"
+            throw e
+        }
+        finally {
+            stage('Allure Report Generation') {
+                allure includeProperties: false,
+                        jdk: '',
+                        results: [[path: 'target/reports/allure-results']]
+            }
+            cleanWs notFailBuild: true
+        }
+    }
 }
