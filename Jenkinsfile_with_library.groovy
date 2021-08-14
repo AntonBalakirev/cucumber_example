97203a8c35c1191dc6e799116477409a4a457d2e
anton_balakirev <anton_balakirev@epage.ru>
2021-08-14 10:35:56 +0300

    add shared pipeline

---
+++ Jenkinsfile_with_library.groovy
@@ -0,0 +1,27 @@
+@Library(example_shared_library) _
+
+def mvnPath = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
+
+node {
+    stage('Checkout SCM') {
+        checkOut(${BRANCH})
+    }
+    stage('Build') {
+        build(mvnPath)
+    }
+    stage('Run Tests') {
+        try {
+            runTestsByTag(mvnPath, ${TAG})
+        }
+        catch (Exception e) {
+            echo "Test run was broken"
+            throw e
+        }
+        finally {
+            stage('Allure Report Generation') {
+                generateAllureReport()
+            }
+            cleanWs notFailBuild: true
+        }
+    }
+}
