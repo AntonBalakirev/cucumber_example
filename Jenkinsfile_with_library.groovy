9670a1f93ac46968e6cc370214a5a9fba06c296f
anton_balakirev <anton_balakirev@epage.ru>
2021-08-14 10:41:03 +0300

    fix shared pipeline

--- Jenkinsfile_with_library.groovy
+++ Jenkinsfile_with_library.groovy
@@ -4,14 +4,14 @@ def mvnPath = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.
 
 node {
     stage('Checkout SCM') {
-        checkOut(${BRANCH})
+        checkOut(params.BRANCH)
     }
     stage('Build') {
         build(mvnPath)
     }
     stage('Run Tests') {
         try {
-            runTestsByTag(mvnPath, ${TAG})
+            runTestsByTag(mvnPath, params.TAG)
         }
         catch (Exception e) {
             echo "Test run was broken"
