281cc02f7f90896756b78fbe7348055bad56de90
anton_balakirev <anton_balakirev@epage.ru>
2022-01-23 04:57:33 +0300

    update selenoid url

--- src/main/java/ru/appline/framework/managers/DriverManager.java
+++ src/main/java/ru/appline/framework/managers/DriverManager.java
@@ -55,12 +55,12 @@ public class DriverManager {
             case "remote":
                 DesiredCapabilities capabilities = new DesiredCapabilities();
                 capabilities.setBrowserName("chrome");
-                capabilities.setVersion("73.0");
+                capabilities.setVersion("81.0");
                 capabilities.setCapability("enableVNC", true);
                 capabilities.setCapability("enableVideo", false);
                 try {
                     driver = new RemoteWebDriver(
-                            URI.create("http://selenoid.appline.ru:4445/wd/hub/").toURL(),
+                            URI.create("http://164.92.227.174/:4444/wd/hub/").toURL(),
                             capabilities);
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
